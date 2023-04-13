e7a172937131d97f21df0df604fd845efb9acbe1
Gustavo <gustavo.okamoto@unicesumar.edu.br>
2023-04-13 19:07:01 -0300

    Form teste

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [texto, setTexto] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label htmlFor='input'>Escreva algo: </label>
+        <input 
+          id='input'
+          value={texto}
+          name="texto"
+          onChange={(e) => setTexto(e.target.value)}
+        />
+      </form>
+
+      <p>{texto}</p>
     </div>
   );
 }
